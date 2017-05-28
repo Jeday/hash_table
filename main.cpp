2b78b83f0a7587d976726e6530d5833b3d549e41
Evgeny Taktarov <Jeday@users.noreply.github.com>
2017-05-28 03:00:18 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "hash.h"
+#include <functional>
+#include <string>
+
+class eq{
+public:
+    bool operator()(const std::string& s1,const std::string& s2){
+        return s1==s2;
+    }
+
+    };
 
 
 int main(int argc, char *argv[])
 {
+
+    test_hash();
+
     return 0;
 }
